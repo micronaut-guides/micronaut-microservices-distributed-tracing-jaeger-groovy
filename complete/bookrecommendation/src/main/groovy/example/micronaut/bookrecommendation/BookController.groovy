3ef6b4083d8a88bd2dce58bb11e497f4978e97bf
Iván López <lopezi@objectcomputing.com>
2019-11-19 10:42:44 +0100

    Minor cleanup

--- complete/bookrecommendation/src/main/groovy/example/micronaut/bookrecommendation/BookController.groovy
+++ complete/bookrecommendation/src/main/groovy/example/micronaut/bookrecommendation/BookController.groovy
@@ -6,25 +6,25 @@ import io.micronaut.http.annotation.Get
 import io.reactivex.Flowable
 
 @CompileStatic
-@Controller("/books") // <1>
+@Controller("/books")
 class BookController {
 
     private final BookCatalogueOperations bookCatalogueOperations
     private final BookInventoryOperations bookInventoryOperations
 
     BookController(BookCatalogueOperations bookCatalogueOperations,
-                          BookInventoryOperations bookInventoryOperations) { // <2>
+                   BookInventoryOperations bookInventoryOperations) {
         this.bookCatalogueOperations = bookCatalogueOperations
         this.bookInventoryOperations = bookInventoryOperations
     }
 
-
-    @Get("/") // <3>
+    @Get("/")
     Flowable<BookRecommendation> index() {
         return bookCatalogueOperations.findAll()
-                .flatMapMaybe { b -> bookInventoryOperations.stock(b.isbn)
-                        .filter { hasStock -> hasStock == Boolean.TRUE }
-                        .map {rsp -> b }
-        }.map { book -> new BookRecommendation(book.name) }
+            .flatMapMaybe { b ->
+                bookInventoryOperations.stock(b.isbn)
+                    .filter { hasStock -> hasStock == Boolean.TRUE }
+                    .map { rsp -> b }
+            }.map { book -> new BookRecommendation(book.name) }
     }
 }
